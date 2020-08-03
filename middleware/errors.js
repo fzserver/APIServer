8eade589f6820ce91875db50cbc8858161b530a9
Parth Aggarwal <frazile.com@gmail.com>
2020-08-03 08:32:36 +0530

    Mongoose Validation Error Handling

--- middleware/errors.js
+++ middleware/errors.js
@@ -4,12 +4,24 @@ const errorHandler = (err, req, res, next) => {
   error.message = err.message;
 
   // Log to console for dev
-  console.log(err.stack.red);
+  console.log(err);
 
   // Mongoose Bad objectID
   if (err.name === "CastError") {
     const message = `Resource not found with id of ${err.value}`;
     error = new ErrorResponse(message, 404);
+  }
+
+  // Mongoose Duplicate Key
+  if (err.code === 11000) {
+    const message = "Duplicate field value entered";
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Mongoose Validation Error
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors);
+    error = new ErrorResponse(message, 400);
   }
 
   res
