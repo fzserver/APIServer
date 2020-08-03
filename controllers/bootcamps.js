f16343c222df6faae0909c658478d59bfa85758c
Parth Aggarwal <frazile.com@gmail.com>
2020-08-03 08:10:17 +0530

    Mongoose Error Handling

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -11,11 +11,8 @@ exports.getBootcamps = async (req, res, next) => {
       .status(200)
       .json({ success: true, count: bootcamps.length, data: bootcamps });
   } catch (err) {
-    res.status(400).json({ success: false });
+    next(err);
   }
-  // res
-  //   .status(200)
-  //   .json({ success: true, msg: "Show all bootcamps", hello: req.hello });
 };
 
 // @desc    Get Single Bootcamp
@@ -31,25 +28,19 @@ exports.getBootcamp = async (req, res, next) => {
     }
     res.status(200).json({ success: true, data: bootcamp });
   } catch (err) {
-    // res.status(400).json({ success: false });
-    next(
-      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
-    );
+    next(err);
   }
-  // res.status(200).json({ success: true, msg: `Get bootcamp ${req.params.id}` });
 };
 
 // @desc    Create new bootcamp
 // @route   POST /api/v1/bootcamps
 // @access  Private
 exports.createBootcamp = async (req, res, next) => {
-  // console.log(req.body);
-  // res.status(200).json({ success: true, msg: "Create new bootcamp" });
   try {
     const bootcamp = await Bootcamp.create(req.body);
     res.status(201).json({ success: true, data: bootcamp });
   } catch (err) {
-    res.status(400).json({ success: false });
+    next(err);
   }
 };
 
@@ -64,16 +55,15 @@ exports.updateBootcamp = async (req, res, next) => {
     });
 
     if (!bootcamp) {
-      return res.status(400).json({ success: false });
+      return next(
+        new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+      );
     }
 
     res.status(200).json({ success: true, data: bootcamp });
   } catch (err) {
-    res.status(400).json({ success: false });
+    next(err);
   }
-  // res
-  //   .status(200)
-  //   .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
 };
 
 // @desc    Delete bootcamp
@@ -83,16 +73,15 @@ exports.deleteBootcamp = async (req, res, next) => {
   try {
     const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
     if (!bootcamp) {
-      return res.status(400).json({ success: false });
+      return next(
+        new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+      );
     }
     res.status(200).json({
       success: true,
       data: { message: `Bootcamp Deleted` },
     });
   } catch (err) {
-    res.status(400).json({ success: false });
+    next(err);
   }
-  // res
-  //   .status(200)
-  //   .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
 };
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const dotenv = require("dotenv");
-// const logger = require("./middleware/logger");
 const morgan = require("morgan");
 const colors = require("colors");
 const errorHandler = require("./middleware/errors");
@@ -25,8 +24,6 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-// app.use(logger);
-
 // Mount Routers
 app.use("/api/v1/bootcamps", bootcamps);
 
@@ -47,59 +44,3 @@ process.on("unhandledRejection", (err, promise) => {
   // Close server & exit process
   server.close(() => process.exit(1));
 });
-
-// const http = require('http');
-
-// const todos = [
-//     {id: 1, name: 'Parth'},
-//     {id: 2, name: 'Sahil'},
-//     {id: 3, name: 'Surya'}
-// ]
-
-// const server = http.createServer((req, res) => {
-//     // console.log(req.headers.authorization);
-//     const { method, url } = req;
-//     let body = [];
-//     req.on('data', chunk => {
-//         body.push(chunk);
-//     }).on('end', () => {
-//         body = Buffer.concat(body).toString();
-
-//         let status = 404;
-//         const response = {
-//             success: false,
-//             data: null,
-//             error: null,
-//         }
-
-//         if(method === 'GET' && url === '/todos') {
-//             status = 200;
-//             response.success = true;
-//             response.data = todos;
-//         } else if(method === 'POST' && url === '/todos') {
-//             const {id, text} = JSON.parse(body);
-
-//             if(!id || !text) {
-//                 status = 400;
-//                 response.error = 'Please add id & text';
-//             } else {
-//                 todos.push({ id, text });
-//                 status = 201;
-//                 response.success = true;
-//                 response.data = todos;
-//             }
-//         }
-
-//         res.writeHead(status, {
-//             'Content-Type': 'application/json',
-//             'X-Powered-By': 'Node.js'
-//         });
-//         res.end(
-//             JSON.stringify(response)
-//         );
-//     });
-
-// });
-
-// const PORT = 8081;
-// server.listen(PORT, () => console.log(`Server Running at ${PORT}!`));
